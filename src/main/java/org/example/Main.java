a10ccd3c51c077a119fa8f694eb95228fea050b5
scrtdf <stepandef@gmail.com>
2023-04-06 17:27:27 +0300

    Initial commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,6 +6,7 @@ public class Main
 {
     public static void main(String[] args)
     {
+        System.out.printf("Practical task 5. Variant 2. Student Kalakutin S.R. Group RIBO-04-21 \n \n");
         Semaphore table = new Semaphore(2);
         for(int i = 1; i<=7; i++){
             new Thread(new StudentRunnable(table), "Student" + i).start();
